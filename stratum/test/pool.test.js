498b93c78a350ccc2f591d16cc7b38275140981c
blinkhash <blinkhashpool@gmail.com>
2023-03-05 11:26:35 -0500

    Simplify validation logic with query updates

--- stratum/test/pool.test.js
+++ stratum/test/pool.test.js
@@ -2945,19 +2945,20 @@ describe('Test pool functionality', () => {
       round: '15c8343c-9751-4c67-aafa-29ff296da484',
       solo: false,
       stale: 0,
-      times: 276.95300000000003,
+      times: 553.906,
       type: 'primary',
-      valid: 22,
-      work: 29.489361720000005
+      valid: 44,
+      work: 58.9787234
     };
     const worker2 = JSON.parse(JSON.stringify(worker1));
     worker2.miner = 'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjQ';
     worker2.worker = 'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjQ.worker4';
     worker2.times = 49.1;
+    worker2.work = 29.489361720000005;
     const expected = {
-      'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjP': { 'immature': 9570.00569668, 'generate': 0 },
-      'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjQ': { 'immature': 429.99430332, 'generate': 0 }};
-    pool.handlePrimaryWorkers([blocks1], [[worker1, worker1, worker2]], false, (results) => {
+      'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjP': { 'immature': 9570.0056964, 'generate': 0 },
+      'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjQ': { 'immature': 429.9943036, 'generate': 0 }};
+    pool.handlePrimaryWorkers([blocks1], [[worker1, worker2]], false, (results) => {
       expect(results).toStrictEqual(expected);
     });
   });
@@ -3547,19 +3548,20 @@ describe('Test pool functionality', () => {
       round: '15c8343c-9751-4c67-aafa-29ff296da484',
       solo: false,
       stale: 0,
-      times: 276.95300000000003,
-      type: 'auxiliary',
-      valid: 22,
-      work: 29.489361720000005
+      times: 553.906,
+      type: 'primary',
+      valid: 44,
+      work: 58.9787234
     };
     const worker2 = JSON.parse(JSON.stringify(worker1));
     worker2.miner = 'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjQ';
     worker2.worker = 'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjQ.worker4';
     worker2.times = 49.1;
+    worker2.work = 29.489361720000005;
     const expected = {
-      'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjP': { 'immature': 9570.00569668, 'generate': 0 },
-      'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjQ': { 'immature': 429.99430332, 'generate': 0 }};
-    pool.handleAuxiliaryWorkers([blocks1], [[worker1, worker1, worker2]], false, (results) => {
+      'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjP': { 'immature': 9570.0056964, 'generate': 0 },
+      'VqspNKCc3ufsUSJb6Vq2TcEVX1Wn9EdHjQ': { 'immature': 429.9943036, 'generate': 0 }};
+    pool.handleAuxiliaryWorkers([blocks1], [[worker1, worker2]], false, (results) => {
       expect(results).toStrictEqual(expected);
     });
   });
